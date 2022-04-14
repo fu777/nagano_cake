97c0c9a5e96ca797be2bc9335abb5b63a6fe4147
EC2 Default User <ec2-user@ip-172-31-20-48.ec2.internal>
2022-04-14 11:32:02 +0000

    [public/cart_item] 3

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,38 +3,39 @@ class Public::CartItemsController < ApplicationController
   def index
     @cart_items = current_customer.cart_items.all
     @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
-    @numbers = *[1..10]
   end
   
   def update
-    @cart_item = CartItem.find(params[:id])
-    @cart_item.update(cart_item_params)
+    @cart_item = current_customer.cart_items
+    @cart_item.update(amount: params[:amount].to_i)
     redirect_to cart_items_path
   end
   
   def destroy
-    @cart_item = CartItem.find(params[:id])
-    @cart_items.destroy
+    @cart_item = current_customer.cart_items
+    # @cart_item = items.find_by(item_id: item.id)
+    @cart_item.destroy
+    @cart_items = current_customer.cart_items.all
     redirect_to cart_items_path
   end
   
   def destroy_all
-    CartItem.destroy_all
+    @cart_items = current_customer.cart_items.all
+    @cart_items.destroy_all
     redirect_to cart_items_path
   end
   
   def create
     @cart_item = current_customer.cart_items.build(cart_item_params)
-    # if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
-    #   @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
-    #   @cart_item.amount += params[:cart_item][:amount].to_i
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item.amount += params[:cart_item][:amount].to_i
       @cart_item.save
       redirect_to cart_items_path
-    # else
-    #   @cart_items = CartItem.all
-    #   @cart_item.save
-    #   redirect_to cart_items_path
-    # end
+    else
+      @cart_item.save
+      redirect_to cart_items_path
+    end
   end
   
   private
