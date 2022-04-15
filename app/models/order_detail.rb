55f785e81b184f11728d487c5d9ce75625baa1f8
EC2 Default User <ec2-user@ip-172-31-20-48.ec2.internal>
2022-04-15 14:14:42 +0000

    [public/order] 2

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,4 +3,9 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
   
+  enum making_status: {"not_manufactured"=>0,
+                "waiting_for_deposit"=>1,
+                "production"=>2,
+                "production_completed"=>3,}
+  
 end
