69f5e305d109fe28325b8af73d353d59b009eb76
EC2 Default User <ec2-user@ip-172-31-20-48.ec2.internal>
2022-04-17 10:06:40 +0000

    [public/order] 5

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -12,12 +12,33 @@ class Public::OrdersController < ApplicationController
     @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
     @shipping_cost = 800
     @total_payment = @total + @shipping_cost
+    @order = Order.new(order_params)
+    if params[:order][:select_address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:select_address] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    else
+      @order.postal_code = params[:postal_code]
+      @order.address = params[:address]
+      @order.name = params[:name]
+    end
   end
 
   def complete
+    
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.save
     @cart_items = current_customer.cart_items.all
       @cart_items.each do |cart_item|
-        @order_details = @order.order_items.new
+        @order_details = @order.order_details.new
         @order_details.item_id = cart_item.item.id
         @order_details.image = cart_item.item.image
         @order_details.name = cart_item.item.name
@@ -29,25 +50,8 @@ class Public::OrdersController < ApplicationController
         redirect_to complete_path
         current_customer.cart_items.destroy_all
       end
-  end
-
-  def create
-    @order = Order.new(order_params)
-    if params[:select_address] == 0
-      @order.postal_code = current_customer.postal_code
-      @order.address = current_customer.address
-      @order.name = current_customer.first_name + current_customer.last_name
-    elsif params[:select_address] == "1"
-      @address = Address.find(params[:order][:address_id])
-      @order.postal_code = @address.postal_code
-      @order.address = @address.address
-      @order.name = @address.name
-    else
-      @order.postal_code = params[:postal_code]
-      @order.address = params[:address]
-      @order.name = params[:name]
-    end
-    redirect_to confirm_path
+    
+    redirect_to complete_path
   end
 
   def index
