2f2f5745ecf48377ffcc6b1def8d0b64336d2286
EC2 Default User <ec2-user@ip-172-31-20-48.ec2.internal>
2022-04-19 10:21:52 +0000

    [public/item] 2

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -21,14 +21,23 @@ class Admin::ItemsController < ApplicationController
   
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to admin_items_path
+    @genres = Genre.all
+    if @item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
   end
   
   def update
     @item = Item.find(params[:id])
-    @item.update(item_params)
-    redirect_to admin_item_path(@item.id)
+    @genres = Genre.all
+    @genre = Genre.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
+    else
+      render :edit
+    end
   end
   
   private
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,18 +1,18 @@
 class Public::ItemsController < ApplicationController
-  
+
   def index
-    @items = Item.all
+    @items = Item.where(is_active: true)
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem
   end
-  
+
   private
-  
+
   def item_params
     params.require(:item).permit(:image, :genre_id, :name, :introduction, :price, :is_active)
   end
-  
+
 end
