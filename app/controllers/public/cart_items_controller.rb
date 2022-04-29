e00571e54f099f0870f5b0a94cd9ec4e2a7242fd
EC2 Default User <ec2-user@ip-172-31-20-48.ec2.internal>
2022-04-29 15:09:00 +0000

    [customer/sign] 4

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,7 +5,6 @@ class Public::CartItemsController < ApplicationController
   def index
     @cart_items = current_customer.cart_items.all
     @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
-    # @new_cart_item = CartItem.new
   end
 
   def update
